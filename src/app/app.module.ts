3da1e5c583ff13ab6793afa90a8a7ce6d2c430b2
brexite <mblackmanwatts@gmail.com>
2022-09-14 18:03:47 +0100

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { ToastrModule } from 'ngx-toastr';
+import { AngularFireModule } from '@angular/fire/compat';
+// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+// import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+// import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+const config = {
+  apiKey: 'AIzaSyAMWPxYMMFB_8JDGq3Fb0SHV8IEcQBU-SA',
+  authDomain: 'acc-admin-tools.firebaseapp.com',
+  projectId: 'acc-admin-tools',
+  storageBucket: 'acc-admin-tools.appspot.com',
+  messagingSenderId: '420480812472',
+  appId: '1:420480812472:web:d426dd6fb49466e372fb4d',
+  measurementId: 'G-FGEPV6YTNK',
+};
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,12 +32,16 @@ import { ToastrModule } from 'ngx-toastr';
     SharedModule,
     ClipboardModule,
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp(config),
+    // AngularFirestoreModule, // firestore
+    // AngularFireAuthModule, // auth
+    // AngularFireStorageModule // storage
     ToastrModule.forRoot({
-        timeOut: 2500,
-        positionClass: 'toast-top-right'
-    })
+      timeOut: 2500,
+      positionClass: 'toast-top-right',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
