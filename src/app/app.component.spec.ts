505d7549865aa02318e640401e50057d46e752c5
brexite <mblackmanwatts@gmail.com>
2022-09-14 18:49:32 +0100

    fixed the fucking firebase shit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'acc-admin-tool'`, () => {
+  it(`should have as title 'acc-admin-tools'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('acc-admin-tool');
+    expect(app.title).toEqual('acc-admin-tools');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('acc-admin-tool app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'acc-admin-tools app is running!'
+    );
   });
 });
