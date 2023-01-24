76ed9c6a5b4cdc77406980a213eba3e9179b6130
brexite <mblackmanwatts@gmail.com>
2023-01-24 14:26:26 +0000

    doAdminsExist needs to be set back to false

--- src/app/entrylist-editor/entrylist-editor.component.ts
+++ src/app/entrylist-editor/entrylist-editor.component.ts
@@ -437,6 +437,7 @@ export class EntrylistEditorComponent implements OnInit {
   fileHandler(file: File) {
     if (file) {
       this.loading = true;
+      this.doAdminsExist = false;
 
       const fileReader = new FileReader();
       fileReader.readAsText(file, 'UTF-8');
@@ -480,6 +481,7 @@ export class EntrylistEditorComponent implements OnInit {
 
     this.driverLength = this.json.entries.length;
     this.patchForm(0);
+    this.doAdminsExist = false;
     this.loading = false;
   }
 
