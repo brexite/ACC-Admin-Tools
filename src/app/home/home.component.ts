f404cdb98e3b1c9500807296b466553c67465678
brexite <mblackmanwatts@gmail.com>
2023-02-12 20:01:05 +0000

    Version number display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { fadeInOut } from "../shared/animations/animations";
 import { Clipboard } from '@angular/cdk/clipboard';
+import packageJson  from '../../../package.json';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,7 @@ export class HomeComponent implements OnInit {
   outputForm: FormGroup;
   loading: boolean = true;
   output: string;
+  version: string = packageJson.version;
 
   bg = [
     './assets/bg0.png',
